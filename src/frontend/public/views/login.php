35c9c114cdfc835bdb16d639523e1f26ee8993d7
Michal Janata <mjanata@gmail.com>
2017-07-12 13:39:38 +0200

    Překlad do AJ

--- src/frontend/public/views/login.php
+++ src/frontend/public/views/login.php
@@ -5,7 +5,7 @@ if ( ! isUserLoggedInSkautis() ) {
 	?>
 	<div class="wp-core-ui" style="text-align: center;">
 		<a class="button button-primary button-hero pic-lilie" href="<?php echo getSkautisRegisterUrl(); ?>">
-			Přihlásit se přes skautIS
+			<?php _e( 'Log in with skautIS', 'skautis-integration' ); ?>
 		</a>
 	</div>
 	<?php
@@ -15,7 +15,7 @@ if ( ! isUserLoggedInSkautis() ) {
 		<strong>Jste přihlášeni ve skautISu</strong>
 		<br/>
 		<a class="button" href="<?php echo getSkautisLogoutUrl(); ?>">
-			Odhlásit se ze skautISu
+			<?php _e( 'Log out of skautIS', 'skautis-integration' ); ?>
 		</a>
 	</div>
 	<?php
