2a940202b11e01faf123ea6b983d01de9d400b86
Michal Janata <mjanata@gmail.com>
2017-07-14 08:43:34 +0200

    Verze 1.1

--- skautis-integration.php
+++ skautis-integration.php
@@ -3,7 +3,7 @@
  * Plugin Name:       skautIS integration
  * Plugin URI:        https://github.com/skaut/skautis-integration
  * Description:       Integrace WordPressu se skautISem
- * Version:           1.0.1-beta
+ * Version:           1.1
  * Author:            David Odehnal
  * Author URI:        https://davidodehnal.cz/
  * Text Domain:       skautis-integration
