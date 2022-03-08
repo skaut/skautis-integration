d4995ca4538492eb03ef986f735288b341061da8
Marek Dědič <developer@dedic.eu>
2022-03-08 19:03:03 +0100

    Added types for JQuery.repeater

---
+++ src/d.ts/jquery.repeater.d.ts
@@ -0,0 +1,17 @@
+interface JQueryRepeaterOptions {
+	initEmpty?: boolean;
+	defaultValues?: Record<string, any>;
+	show?: () => void;
+	hide?: (deleteElement: () => void) => void;
+	ready?: (setIndexes: () => void) => void;
+	isFirstItemUndeletable?: boolean;
+}
+
+interface JQueryRepeater {
+	(fig: JQueryRepeaterOptions): JQuery;
+	setList(rows: Array<Record<string, any>>): void
+}
+
+interface JQuery {
+	repeater: JQueryRepeater;
+}
