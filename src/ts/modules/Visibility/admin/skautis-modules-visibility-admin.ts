72816f224ea56d11be7a0e8055bce6992b2adf62
Marek Dědič <developer@dedic.eu>
2022-03-11 12:41:18 +0100

    Fixed easy lint issues in skautis-modules-visibility-admin

--- src/ts/modules/Visibility/admin/skautis-modules-visibility-admin.ts
+++ src/ts/modules/Visibility/admin/skautis-modules-visibility-admin.ts
@@ -1,71 +1,79 @@
-(function ($) {
-	'use strict';
+( function ( $ ): void {
+	const $repeater = $( '#repeater_post' );
 
-	var $repeater = $('#repeater_post');
-
-	if ($repeater.length) {
-		$repeater.repeater({
-			initEmpty: true,
-			defaultValues: {
-				'role': $('select[name="skautis-integration_rules"]').first().find('option:selected').val()
-			},
-			show: function () {
-				$(this).slideDown(150);
-				updateAvailableOptions();
-			},
-			hide: function (deleteElement) {
-				$(this).slideUp(150, deleteElement);
-				setTimeout(function () {
+	if ( $repeater.length ) {
+		$repeater
+			.repeater( {
+				initEmpty: true,
+				defaultValues: {
+					role: $( 'select[name="skautis-integration_rules"]' )
+						.first()
+						.find( 'option:selected' )
+						.val(),
+				},
+				show: function () {
+					$( this ).slideDown( 150 );
 					updateAvailableOptions();
-				}, 250);
-			},
-			ready: function () {
-				reinitSelect2();
-			},
-			isFirstItemUndeletable: true
-		}).setList(window.rulesData ?? []);
-
+				},
+				hide: function ( deleteElement ) {
+					$( this ).slideUp( 150, deleteElement );
+					setTimeout( function () {
+						updateAvailableOptions();
+					}, 250 );
+				},
+				ready: function () {
+					reinitSelect2();
+				},
+				isFirstItemUndeletable: true,
+			} )
+			.setList( window.rulesData ?? [] );
 	} else {
 		reinitSelect2();
 	}
 
-	function reinitSelect2() {
-		jQuery('select.select2').select2({
-			placeholder: 'Vyberte pravidlo...'
-		}).on('change.skautis_modules_visibility_admin', updateAvailableOptions);
+	function reinitSelect2(): void {
+		jQuery( 'select.select2' )
+			.select2( {
+				placeholder: 'Vyberte pravidlo...',
+			} )
+			.on(
+				'change.skautis_modules_visibility_admin',
+				updateAvailableOptions
+			);
 	}
 
-	function updateAvailableOptions() {
-		var usedOptions: Array<string> = [];
+	function updateAvailableOptions(): void {
+		const usedOptions: Array< string > = [];
 
-		setTimeout(function () {
+		setTimeout( function () {
+			const $selectRules = jQuery( 'select.rule' );
 
-			var $selectRules = jQuery('select.rule');
+			$selectRules.each( function () {
+				usedOptions.push( jQuery( this ).val() as string );
+			} );
 
-			$selectRules.each(function () {
-				usedOptions.push(jQuery(this).val() as string);
-			});
+			$selectRules.find( 'option' ).removeAttr( 'disabled' );
 
-			$selectRules.find('option').removeAttr('disabled');
+			const $rulesUsedInParents = jQuery(
+				'#skautis_modules_visibility_parentRules'
+			).find( 'li[data-rule]' );
+			$rulesUsedInParents.each( function () {
+				usedOptions.push( jQuery( this ).data( 'rule' ) );
+			} );
 
-			var $rulesUsedInParents = jQuery('#skautis_modules_visibility_parentRules').find('li[data-rule]');
-			$rulesUsedInParents.each(function () {
-				usedOptions.push(jQuery(this).data('rule'));
-			});
-
-			for (var key in usedOptions) {
-				if (usedOptions.hasOwnProperty(key)) {
-					$selectRules.find('option[value="' + usedOptions[key] + '"]').attr('disabled', 'disabled');
-				}
+			for ( const item of usedOptions ) {
+				$selectRules
+					.find( 'option[value="' + item + '"]' )
+					.attr( 'disabled', 'disabled' );
 			}
 
-			$selectRules.each(function () {
-				jQuery(this).find('option:selected').removeAttr('disabled');
-			});
+			$selectRules.each( function () {
+				jQuery( this )
+					.find( 'option:selected' )
+					.removeAttr( 'disabled' );
+			} );
 
 			reinitSelect2();
-
-		}, 0);
+		}, 0 );
 	}
-
-})(jQuery);
+} )( jQuery );
