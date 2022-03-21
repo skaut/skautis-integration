fabc3d754a2f15b4ecdc41d38bdde87b14e919a8
Marek Dědič <developer@dedic.eu>
2022-03-21 16:45:23 +0100

    Metabox.php rename

--- src/skautis-integration.php
+++ src/skautis-integration.php
@@ -62,7 +62,7 @@ class SkautisIntegration {
 		require __DIR__ . '/src/modules/Shortcodes/frontend/class-frontend.php';
 		require __DIR__ . '/src/modules/Shortcodes/Shortcodes.php';
 		require __DIR__ . '/src/modules/Visibility/admin/class-admin.php';
-		require __DIR__ . '/src/modules/Visibility/admin/Metabox.php';
+		require __DIR__ . '/src/modules/Visibility/admin/class-metabox.php';
 		require __DIR__ . '/src/modules/Visibility/admin/class-settings.php';
 		require __DIR__ . '/src/modules/Visibility/frontend/class-frontend.php';
 		require __DIR__ . '/src/modules/Visibility/class-visibility.php';
