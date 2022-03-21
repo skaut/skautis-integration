c611bb53b65caa43da880b29934b49d744f6d08b
Marek Dědič <developer@dedic.eu>
2022-03-21 16:13:02 +0100

    Func.php rename

--- src/skautis-integration.php
+++ src/skautis-integration.php
@@ -74,7 +74,7 @@ class SkautisIntegration {
 		require __DIR__ . '/src/rules/IRule.php';
 		require __DIR__ . '/src/rules/class-revisions.php';
 		require __DIR__ . '/src/rules/Rule/All.php';
-		require __DIR__ . '/src/rules/Rule/Func.php';
+		require __DIR__ . '/src/rules/Rule/class-func.php';
 		require __DIR__ . '/src/rules/Rule/class-role.php';
 		require __DIR__ . '/src/rules/Rule/class-membership.php';
 		require __DIR__ . '/src/rules/Rule/Qualification.php';
